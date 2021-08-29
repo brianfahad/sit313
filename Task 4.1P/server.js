64fa30cee82bd99b084b29f71749a807d990a077
Brian Fahad <brianifahad@gmail.com>
2021-08-29 18:02:26 +1000

    Added error validation to 4.1P

--- Task 4.1P/server.js
+++ Task 4.1P/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const db = require("mongoose");
 const sendGridMail = require("@sendgrid/mail");
 const fs = require('fs');
-
+const { body, validationResult } = require('express-validator');
 const app = express();
 const PORT = 8080;
 
@@ -22,58 +22,85 @@ app.get('/', (req, res) => {
 })
 
 // Post data to the user model and send email
-app.post('/register', async (req, res) => {
+app.post('/register',
+    body('country_of_residence').isLength({ min: 1 }).withMessage('Country of residence is required.'),
+    body('first_name').isLength({ min: 1 }).withMessage('First name is required.'),
+    body('last_name').isLength({ min: 1 }).withMessage('Last name is required.'),
+    body('email').isLength({ min: 1 }).withMessage('Email is required.').isEmail().withMessage('Email is invalid'),
+    body('address_line_1').isLength({ min: 1 }).withMessage('Address is required.'),
+    body('city').isLength({ min: 1 }).withMessage('City is required.'),
+    body('state').isLength({ min: 1 }).withMessage('State is required.'),
+    body('password').isLength({ min: 8 }).withMessage('Password must be 8 characters or more.').custom((match, { req }) => {
+        if (match !== req.body.confirm_password) {
+            throw new Error("Passwords do not match");
+        } else {
+            return value;
+        }
+    }),
+    async (req, res) => {
 
-    const { country_of_residence, first_name, last_name, email, password, address_line_1, address_line_2, city, state, zip } = req.body;
-
-    try {
-        db.connect('MONGO_DB_CONNECTION_URL', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("-- Connection to DB is successful --")
-
-        const newUser = new User({
-            country_of_residence,
-            first_name,
-            last_name,
-            email,
-            password,
-            address: {
-                address_line_1,
-                address_line_2,
-                city,
-                state,
-                zip
-            }
-        })
-
-        const sg_email = {
-            to: email,
-            from: {
-                name: 'iService Team',
-                email: 'bfahad@deakin.edu.au',
-            },
-            subject: 'Welcome to iService!',
-            html: fs.readFileSync(__dirname + '/public/emails/welcome.html', 'utf-8'),
+        /*
+        * Error validation
+        */
+        const errorFormatter = ({ location, msg, param, value, nestedErrors }) => {
+            return `${msg}`;
+        };
+        const errors = validationResult(req).formatWith(errorFormatter);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.mapped() });
         }
 
-        await newUser.save();
+        const { country_of_residence, first_name, last_name, email, password, confirm_password, address_line_1, address_line_2, city, state, zip, phone_number } = req.body;
 
-        console.log("-- User created successfully --");
+        try {
+            db.connect('MONGO_DB_CONNECTION_URL', {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            });
+            console.log("-- Connection to DB is successful --")
 
-        res.send("User created successfully");
+            const newUser = new User({
+                country_of_residence,
+                first_name,
+                last_name,
+                email,
+                password,
+                address: {
+                    address_line_1,
+                    address_line_2,
+                    city,
+                    state,
+                    zip
+                },
+                phone_number
+            })
 
-        sendGridMail.send(sg_email).then(res => {
-            console.log('-- Email sent successfully --')
-        }).catch(err => {
-            console.log(err.message)
-        });
+            const sg_email = {
+                to: email,
+                from: {
+                    name: 'iService Team',
+                    email: 'bfahad@deakin.edu.au',
+                },
+                subject: 'Welcome to iService!',
+                html: fs.readFileSync(__dirname + '/public/emails/welcome.html', 'utf-8'),
+            }
 
-    } catch (err) {
-        console.log(err)
-    }
-})
+            await newUser.save();
+
+            console.log("-- User created successfully --");
+
+            res.send("User created successfully");
+
+            sendGridMail.send(sg_email).then(res => {
+                console.log('-- Email sent successfully --')
+            }).catch(err => {
+                console.log(err.message)
+            });
+
+        } catch (err) {
+            console.log(err)
+        }
+    })
 
 app.listen(PORT, (req, res) => {
     console.log("Server is running on " + PORT)
